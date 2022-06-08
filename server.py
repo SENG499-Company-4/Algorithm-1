fa7c23586a7563ab7b58fb1d5ee48efe4f8b2ee0
Dana <danabell@uvic.ca>
2022-06-07 19:03:24 -0700

    Add typed Preference and Professor classes

--- server.py
+++ server.py
@@ -1,23 +1,29 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 app = FastAPI()
 
 
-class Teacher(BaseModel):
-    name: str
+class Preference(BaseModel):
+    course_num: str
+    preference_num: int
+    term: str
 
 
-class CourseData(BaseModel):
-    teachers: list[Teacher]
+class Professor(BaseModel):
+    preference_list: list[Preference]
+    display_name: str
+    fall_term_courses: int
+    spring_term_courses: int
+    summer_term_courses: int
 
 
-class Schedule(BaseModel):
-    times: list[str]
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-
-@app.post("/", response_model=Schedule)
-async def root(data: CourseData):
+@app.post("/", response_model=Professor)
+async def root(data: Preference):
     print(data)
-
-    return Schedule(["10:30", "11:30"])
+    return Professor(["10:30", "11:30"])
