5918fdf792f9d96952a02dcadbfc10ae55374878
Dana <danabell@uvic.ca>
2022-06-09 19:41:35 -0700

    Modify process function to output as json

--- process.py
+++ process.py
@@ -1,34 +1,51 @@
 """
-Processes spreadsheet file to parse out preferences into pandas dataframe
-
+Processes preferences spreadsheet file
 """
 
 import pandas as pd
 from server import Preference, Professor
 import itertools
+import json
 
-def process_spreadsheeet() -> list[Professor]:
-    """
-    Turns spreadsheets into dataframes
-    
-    Return list of Prof-Preference objects
-    """
-    df = pd.read_excel("tp.xlsx")
-    df = df.iloc[0:33,:25] # filter out the excess data
 
-    prof_list = []
+class Process:
+    def process_spreadsheet():
+        """
+        Turns spreadsheets into dataframes
 
-    for row in df.itertuples():
-        # create preference list
-        pref_list = []  
-        # misc data shaping
-        d = row._asdict()
-        d = dict(itertools.islice(d.items(),2, len(d.items())))
-        # iterate through row in spreadsheet
-        for course in d:
-            Preference(course, d[course], "") # create a new preference
-        prof = row.Instructor
-        pref_list.append(Preference)
-        prof_list.append(Professor(pref_list, prof, 0,0,0))
-        
-    return prof_list
+        Return list of Prof-Preference objects
+        """
+        df = pd.read_excel("tp.xlsx")
+        df = df.iloc[0:33, :25]  # filter out the excess data
+
+        prof_list = []
+
+        for row in df.itertuples():
+            # create preference list
+            pref_list = []
+            # misc data shaping
+            d = row._asdict()
+            d = dict(itertools.islice(d.items(), 2, len(d.items())))
+            # iterate through rows in spreadsheet
+            for course in d:
+                pref_data = {
+                    "course_num": course,
+                    "preference_num": d[course],
+                    "term": "",
+                }
+                pref_list.append(Preference(**pref_data))  # create a new preference
+            prof = row.Instructor
+            prof_data = {
+                "preference_list": pref_list,
+                "display_name": prof,
+                "fall_term_courses": 0,
+                "spring_term_courses": 0,
+                "summer_term_courses": 0,
+            }
+            prof_list.append(Professor(**prof_data))
+
+        li = repr(prof_list)
+        json_str = json.dumps(li)
+        # with open("output.json", "w") as f:
+        #    f.write(json_str)
+        return prof_list
--- server.py
+++ server.py
@@ -6,26 +6,45 @@ app = FastAPI()
 
 
 class Preference(BaseModel):
-    course_num: str      # ex. CSC 226
+    course_num: str  # ex. CSC 226
     preference_num: int  # ex. 6
-    term: str            # ex. fall
+    term: str  # ex. fall
+
+    def __repr__(self):
+        return repr(
+            {
+                "course_num": self.course_num,
+                "preference_num": self.preference_num,
+                "term": self.term,
+            }
+        )
 
 
 class Professor(BaseModel):
     preference_list: list[Preference]
-    display_name: str          # ex. Michael Zastre
-    fall_term_courses: int     # ex. 2
-    spring_term_courses: int   # ex. 1
-    summer_term_courses: int   # ex. 0
+    display_name: str  # ex. Michael Zastre
+    fall_term_courses: int  # ex. 2
+    spring_term_courses: int  # ex. 1
+    summer_term_courses: int  # ex. 0
+
+    def __repr__(self):
+        return repr(
+            {
+                "preference_list": self.preference_list,
+                "display_name": self.display_name,
+                "fall_term_courses": self.fall_term_courses,
+                "spring_term_courses": self.spring_term_courses,
+                "summer_term_courses": self.summer_term_courses,
+            }
+        )
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+
 @app.post("/", response_model=Professor)
 async def root(data: Preference):
     print(data)
     return Professor(["10:30", "11:30"])
-
-
