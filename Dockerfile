67d261b8a2d8ce49439f3952bf1f10cdae91ce08
Tristan Slater <hello@trslater.ca>
2022-06-16 11:29:07 -0700

    Get environment defaults from '.env' file

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ COPY ./requirements.txt /code/
 RUN pip install --no-cache-dir --upgrade pip && \
     pip install --no-cache-dir -r requirements.txt
 
+COPY ./.env /code/
 COPY ./api /code/api
 COPY ./tp.xlsx /code
 
--- api/run.py
+++ api/run.py
@@ -1,11 +1,13 @@
 import os
 
+from dotenv import load_dotenv
 import uvicorn
 
 from .main import app
 
 
 if __name__ == "__main__":
+    load_dotenv()
     uvicorn.run(app,
-                host=os.environ.get("HOST", "0.0.0.0"),
-                port=int(os.environ.get("PORT", 8000)))
+                host=os.environ["HOST"],
+                port=int(os.environ["PORT"]))
