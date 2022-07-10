cf96bf89f8df23356bc9cfe710026e3bc77060c1
tyellison <48420980+tyellison@users.noreply.github.com>
2022-07-09 21:26:21 -0700

    validating schedule (with times) by checking every course has 0-1 professors, no professor is assigned more courses than their set availability, and no professor teaches two courses at the same time

--- rl/randopt/randopt_ex.py
+++ rl/randopt/randopt_ex.py
@@ -15,9 +15,9 @@ def main():
     courses = 33
     times = 51
     teachers = 29
-    dims = (courses, times, teachers)
+    dims = {"courses":courses, "times":times, "teachers":teachers}
 
-    #"""
+    """
     with open("prefs.csv", "r") as fprefs:
         prefs = np.loadtxt("prefs.csv", delimiter=",", dtype=np.int16)
 
@@ -25,16 +25,18 @@ def main():
     for i in range(rows):
         for j in range(cols):
             prefs[i, j] = val_map[prefs[i, j]]
-    #"""
+    """
     
-    #prefs = np.random.randint(7, size=(teachers, courses), dtype=np.uint8)
+    prefs = np.random.randint(7, size=(teachers, courses), dtype=np.uint8)
     avails = np.array([3 for i in range(teachers)], dtype=np.uint8)
-    max_iter = 10
+    max_iter = 1000
     P = [0,1,2,3,4,5,6]
-    p_mean = 3
+    p_tgt = 3
 
-    ro = RandOpt(dims, prefs, avails, max_iter, P, 4)
+    ro = RandOpt(dims, prefs, avails, max_iter, P, p_tgt)
     ro.solve()
+    print(f"connections: {ro.sparse()}\nvalid: {ro.is_valid_schedule()}")
+    ro.plot()
 
 
 if __name__ == "__main__":
